57472b0c792458487004b04a1a07e4eaeaee429f
Saatvik <10013037@sbstudents.org>
2019-06-11 08:07:19 -0400

    Partner A added comments about what he learned

--- CombinedRunner.java
+++ CombinedRunner.java
@@ -3,6 +3,8 @@ public class CombinedRunner implements GitTesting, QuotientInterface{
     public static void main(String[]args){
         GitTesting gitTesting = (int n) -> (Math.sqrt(n)) ==  (int)(Math.sqrt(n));
         System.out.println(gitTesting.perfectSquare(5));
+        //Partner A: I learned how to use lambda functions and how to make a proper workflow on GitKraken.
+        //           I also learned how to use Github
 
         QuotientInterface qi = (a,b) -> System.out.printf("%.3f\n", a/b);
         qi.getQuotient(1000f,50f);
