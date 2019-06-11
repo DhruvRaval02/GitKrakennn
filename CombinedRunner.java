b232242783c209a0554d6efc4c3d6e0777de1fc7
Dhruv Raval <raval.dhruv01@gmail.com>
2019-06-11 08:09:48 -0400

    Partner B added comments about what he learned

--- CombinedRunner.java
+++ CombinedRunner.java
@@ -7,6 +7,10 @@ public class CombinedRunner implements GitTesting, QuotientInterface{
         QuotientInterface qi = (a,b) -> System.out.printf("%.3f\n", a/b);
         qi.getQuotient(1000f,50f);
 
+        //Partner B: Through this project, I learned a lot about how git works, as well as specifics of GitHub and GitKraken.
+        //Partner B: This project taught me how to correctly use the oush, pull, and commit commands, which I had no clue how to use before.
+        //Partner B: Also, this above portion showed me how to correctly use lambda notation in methods, a skill which I think will be pivotal in the future.
+
     }
 
     @Override
